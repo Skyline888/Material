46754aebf64e54508d264e4ff8f52360c2e1e78a
Oleksandr Kalenichenko <O.Kalenichenko@innovecs.com>
2018-12-07 11:28:06 +0200

    profile state

--- src/components/Profile-info.js
+++ src/components/Profile-info.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function Article(props) {
-    const {contentdata} = props;
-    console.log('----', props);
-    const body = <section>{contentdata.text}</section>;
-    return (
-        <div>
-            <h2>{contentdata.title}</h2>
-            {body}
-            <h3>creation date: {(new Date(contentdata.date)).toDateString()}</h3>
-        </div>
-    );
+class Article extends Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isOpen: false
+        };
+
+        // this.handleClick = handleClick.bind(this);
+    }
+
+    // state = {
+    //     isOpen: true
+    // }
+
+    render() {
+        const {contentdata} = this.props;
+        const body = this.state.isOpen && <section>{contentdata.text}</section>;
+        return (
+            <div>
+                <h2>{contentdata.title}</h2>
+                <button onClick={this.handleClick}>
+                    {this.state.isOpen ? 'close' : 'open'}
+                </button>
+                {body}
+                <h3>creation date: {(new Date(contentdata.date)).toDateString()}</h3>
+            </div>
+        );
+    }
+
+    handleClick = () => {
+        console.log('----', 'clicked');
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
 }
 
 export default Article;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, {Component} from "react";
 import Article from './Profile-info';
 import simpledata from '../json-data';
 
-function Content() {
-    return (
-        <div>
-            <h1>App test web</h1>
-            <Article contentdata={simpledata[0]} foo="bar" flag={true} />
-        </div>
-    );
+class Content extends Component {
+    render() {
+        return (
+            <div>
+                <h1>App test web</h1>
+                <Article contentdata={simpledata[0]} foo="bar" flag={true} />
+            </div>
+        );
+    }
+
 }
 
 export default Content;
